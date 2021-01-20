9210e2a9322f854d2584ff20fba45bb3612f5530
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2021-01-20 19:12:03 +0100

    AnswerList component

--- frontend/src/AnswerList.tsx
+++ frontend/src/AnswerList.tsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
 import styled from "styled-components";
-import { keyframes } from "styled-components";
 
+const AnswerListStyle = styled.div`
+  border: 1px solid black;
+  margin: auto;
+  margin-top: 1em;
+  width: 100%;
+`;
 
+const AddAnswerButtonStyle = styled.a`
+  display:block;
+  text-align: center;
+  height:3%;
+  width:3%;
+  padding:1%;
+  color: #E9C46A;
+  border: 0.125em solid #E9C46A;
+  margin: 2em;
+  font-size: 1rem;
+  font-weight: 900;
+  cursor: pointer;
+
+  &:hover {
+    background: #E9C46A;
+    color: #2A9D8F;
+  }
+`;
 
 export interface AnswerListProps
 {
@@ -17,11 +40,17 @@ export class AnswerList extends React.Component<AnswerListProps, {}>
     
   }
 
+  private onClick() { 
+    
+  }
 
   public render()
   {
       return (
-        <p>answer list</p>
+        <AnswerListStyle>
+
+          <AddAnswerButtonStyle onClick={() => this.onClick()}> + </AddAnswerButtonStyle>
+        </AnswerListStyle>
       );
   }
 
